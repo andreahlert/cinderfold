fa5e9f04702b25d9c435995297c4c5a6d06990f5
Andre Ahlert <andre@aex.partners>
2025-08-09 17:33:13 -0300

    Re-export public API in cinderfold/__init__.py; bump to 0.2.0
    Signed-off-by: Andre Ahlert <andre@aex.partners>

--- cinderfold/__init__.py
+++ cinderfold/__init__.py
@@ -1,3 +1,31 @@
 """cinderfold: schema drift detection."""
 
-__version__ = "0.1.0"
+from .diff import Change, diff
+from .dump import dump_sql
+from .filter import filter_changes
+from .fingerprint import fingerprint, short_fingerprint
+from .migrate import migrate
+from .model import Column, ForeignKey, Index, Schema, Table
+from .parser import ParseError, parse
+from .postgres import from_information_schema
+from .render import render
+from .rename import RenameHint, detect_renames
+from .report import to_json, to_markdown, to_text
+from .select import exclude, merge, select
+from .serial import from_dict, from_json, to_dict, to_json as serial_to_json
+from .sql import parse_sql
+from .sqlite import parse_dotschema
+from .stats import Stats, stats
+from .validate import Issue, validate
+
+__version__ = "0.2.0"
+
+__all__ = [
+    "Change", "Column", "ForeignKey", "Index", "Issue", "ParseError",
+    "RenameHint", "Schema", "Stats", "Table",
+    "detect_renames", "diff", "dump_sql", "exclude",
+    "filter_changes", "fingerprint", "from_dict", "from_information_schema",
+    "from_json", "merge", "migrate", "parse", "parse_dotschema", "parse_sql",
+    "render", "select", "serial_to_json", "short_fingerprint", "stats",
+    "to_dict", "to_json", "to_markdown", "to_text", "validate",
+]
